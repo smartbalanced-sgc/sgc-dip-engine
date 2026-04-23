65d5749b5c5d6fcd11a12f5535224c83e798acf2
smartbalanced-sgc <jsiasonj+sgc@gmail.com>
2026-04-23 01:38:42 +0100

    Update dashboard_generator.py

--- src/dashboard_generator.py
+++ src/dashboard_generator.py
@@ -109,6 +109,9 @@ def generate_html(execution_data, macro_regime, vix, portfolio_data, warnings=No
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>SGC Dip Engine</title>
     <style>
         * {{ margin: 0; padding: 0; box-sizing: border-box; }}
